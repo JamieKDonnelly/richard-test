340a20b5f371b5fdbed4a808df77e7c11f359c6b
JamieKDonnelly <jkd-@hotmail.co.uk>
2020-08-27 16:10:32 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -37,10 +37,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
+          <h1>Maybe you're just a noob Rich?</h1>
         </header>
       </div>
     )
